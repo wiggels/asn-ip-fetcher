4a2122f2e77f5c69f4929221f5d9a964b252169d
Hunter Wigelsworth <wiggels@gmail.com>
2024-09-04 02:08:30 -0400

    split v4 and v6 files

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ struct AsnData {
     ip_ranges: Vec<String>,
 }
 
-async fn fetch_asn_data(client: &Client, asn: &str) -> AsnData {
+async fn fetch_asn_data(client: &Client, asn: &str, version: &str) -> AsnData {
     let url: String = format!(
         "https://raw.githubusercontent.com/ipverse/asn-ip/master/as/{}/aggregated.json",
         asn
@@ -25,11 +25,10 @@ async fn fetch_asn_data(client: &Client, asn: &str) -> AsnData {
         let data: Value = serde_json::from_str(&text).unwrap();
         AsnData {
             display_name: data["handle"].as_str().unwrap_or("").to_string(),
-            ip_ranges: data["subnets"]["ipv4"]
+            ip_ranges: data["subnets"][version]
                 .as_array()
                 .unwrap_or(&vec![])
                 .iter()
-                .chain(data["subnets"]["ipv6"].as_array().unwrap_or(&vec![]).iter())
                 .map(|ip: &Value| ip.as_str().unwrap().to_string())
                 .collect(),
         }
@@ -41,16 +40,18 @@ async fn fetch_asn_data(client: &Client, asn: &str) -> AsnData {
     }
 }
 
-async fn fetch_all_asns(asn_list: Vec<String>) -> HashMap<String, AsnData> {
+async fn fetch_all_asns(asn_list: &Vec<String>, version: &str) -> HashMap<String, AsnData> {
+    let version_owned = version.to_string();
     let client: Client = Client::new();
     let mut tasks: Vec<tokio::task::JoinHandle<(String, AsnData)>> = vec![];
-    for asn in &asn_list {
+    for asn in asn_list {
+        let version_clone = version_owned.clone();
         let client_clone: Client = client.clone();
         let asn_clone: String = asn.clone();
         tasks.push(tokio::spawn(async move {
             (
                 asn_clone.clone(),
-                fetch_asn_data(&client_clone, &asn_clone).await,
+                fetch_asn_data(&client_clone, &asn_clone, &version_clone).await,
             )
         }));
     }
@@ -73,8 +74,10 @@ async fn main() {
     let yaml_filename: String =
         env::var("GET_ASN_LIST").unwrap_or_else(|_| "default_asn_list.yml".to_string());
     let asn_list: Vec<String> = read_asn_list_from_yaml(Path::new(&yaml_filename));
-    let asn_data: HashMap<String, AsnData> = fetch_all_asns(asn_list).await;
-
-    let json_file: File = File::create("asn_data.json").unwrap();
-    serde_json::to_writer_pretty(json_file, &asn_data).unwrap();
+    let asn_data_ipv4: HashMap<String, AsnData> = fetch_all_asns(&asn_list, "ipv4").await;
+    let json_file_ipv4: File = File::create("asn_data_ipv4.json").unwrap();
+    serde_json::to_writer_pretty(json_file_ipv4, &asn_data_ipv4).unwrap();
+    let asn_data_ipv6: HashMap<String, AsnData> = fetch_all_asns(&asn_list, "ipv6").await;
+    let json_file_ipv6: File = File::create("asn_data_ipv6.json").unwrap();
+    serde_json::to_writer_pretty(json_file_ipv6, &asn_data_ipv6).unwrap();
 }
